8174628d409017e09c31feeb9ecad43992f77c6e
patrycja-io <patrycjatyra@protonmail.com>
2021-03-29 17:57:19 +0100

    Refactoring & Hooks

--- simple-counter-react-state/src/Counter.js
+++ simple-counter-react-state/src/Counter.js
@@ -1,57 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
+const Counter = ( {max, step } ) => {
+const  [ count, setCount ] = React.useState(0);
 
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
-    this.updateDocumentTitle = this.updateDocumentTitle.bind(this);
-  }
+const increment = () => {
+  setCount(c =>{
+    if (c >= max )return c;
+    return c + step;
+  }); 
+};
 
-  // You'll need to "open in a new window" to see the title update
-  updateDocumentTitle() {
-    document.title = this.state.count;
-  }
-
-  increment() {
-    this.setState((state, props) => {
-      const { max, step } = props;
-      if (state.count >= max) return;
-      return { count: this.state.count + step };
-    }, this.updateDocumentTitle);
-
-    console.log('Before!', this.state);
-  }
-
-  decrement() {
-    this.setState((state, props) => {
-      const { step } = props;
-      if (state.count <= 0) return;
-      return { count: this.state.count - step };
-    }, this.updateDocumentTitle);
-  }
-
-  reset() {
-    this.setState({ count: 0 }, this.updateDocumentTitle);
-  }
-
-  render() {
-    const { count } = this.state;
+const decrement = () => setCount(count - 1);
+const reset = () => setCount(0);
 
     return (
       <main className="Counter">
         <p className="count">{count}</p>
         <section className="controls">
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
-          <button onClick={this.reset}>Reset</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
         </section>
       </main>
     );
-  }
-}
+  };
+
 
 export default Counter;
