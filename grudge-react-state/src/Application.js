905a2a31c3a7c9dce65aa4f0755c1b8460005e1d
patrycja-io <patrycjatyra@protonmail.com>
2021-03-30 18:23:35 +0100

    context and UseContext Hook

--- grudge-react-state/src/Application.js
+++ grudge-react-state/src/Application.js
@@ -1,38 +1,14 @@
-import React, { useReducer, useCallback } from 'react';
+import React from 'react';
 
 import Grudges from './Grudges';
 import NewGrudge from './NewGrudge';
 
 const Application = () => {
-  const [grudges, dispatch ]= useReducer(reducer, initialState);
-
-  const addGrudge =  useCallback(({ person, reason}) => {
-    dispatch({ 
-      type:GRUDGE_ADD,
-      payload: {
-        person, reason, 
-        forgiven:false, 
-        id:id()
-      }
-    });
-   },
-   [dispatch]);
-
-  const toggleForgiveness = useCallback(id =>{
-    dispatch ({ 
-      type: GRUDGE_FORGIVE, 
-      payload: { id }
-    })
-  }, 
-  [dispatch]
-  );
-
-
 
   return (
     <div className="Application">
-      <NewGrudge onSubmit={addGrudge} />
-      <Grudges grudges={grudges} onForgive={toggleForgiveness}/>
+      <NewGrudge/>
+      <Grudges />
     </div>
   );
 };
--- grudge-react-state/src/Grudge.js
+++ grudge-react-state/src/Grudge.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { GrudgeContext } from './GrudgeContext';
 
-const Grudge = React.memo(({ grudge, onForgive }) => {
-  const forgive = () => onForgive(grudge.id);
+const Grudge = React.memo(({ grudge }) => {
+
+  const { toggleForgiveness } = React.useContext(GrudgeContext);
+
+  const forgive = () => toggleForgiveness(grudge.id);
 
   return (
     <article className="Grudge">
