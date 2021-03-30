f56a788a00f218a0b455342837785d6dd6fc139d
patrycja-io <patrycjatyra@protonmail.com>
2021-03-30 16:03:06 +0100

    action state and modfication solution

--- grudge-react-state/src/Application.js
+++ grudge-react-state/src/Application.js
@@ -13,6 +13,12 @@ const reducer = (state, action) => {
  if (action.type === GRUDGE_ADD) {
    return [action.payload, ...state ];
  }
+ if (action.type === GRUDGE_FORGIVE) {
+  return state.map(grudge => {
+     if (grudge.id !== action.payload.id) return grudge;
+     return {...grudge, forgiven:!grudge.forgiven};
+   })
+ }
  return state;
 };
 
@@ -28,13 +34,21 @@ const Application = () => {
         id:id()
       }
     });
-  }
+  };
+
+  const toggleForgiveness = id =>{
+    dispatch ({ 
+      type: GRUDGE_FORGIVE, 
+      payload: { id }
+    })
+  };
+
 
 
   return (
     <div className="Application">
       <NewGrudge onSubmit={addGrudge} />
-      <Grudges grudges={grudges}/>
+      <Grudges grudges={grudges} onForgive={toggleForgiveness}/>
     </div>
   );
 };
